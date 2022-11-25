109eed4a4f0b1af313eec107b7cf0fa4113d5fb1
Francesco Sala <francesco.sala@epfl.ch>
2022-11-25 12:48:21 +0100

    first changes

--- Project_Sala_Viscusi/main.cpp
+++ Project_Sala_Viscusi/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include "fparser.hh"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+
+    // The code must be called specyfing a function
+
+    if (argc > 1) {
+
+        std::cout << "we are gonna use a file" << std::endl;
+    } else
+    {
+        std::cout << "we are gonna use the screen to print the solution" << std::endl;
+    }
+
+
+    FunctionParser *fp_RHS;
+
+    fp_RHS->Parse(argv[1], "t,y");
+    double v[2] = {
+            ,1};
+    double result = fp_RHS->Eval(v);
+
+    std::cout << result << std::endl;
     return 0;
 }
